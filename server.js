34c67d9c2b04bf1ad25c2b38d1eb485d03bdcc7c
Lucas Chagas <lchagas@umass.edu>
2020-05-05 03:01:16 -0400

    Fixed Port Number in server.js
    Signed-off-by: Lucas Chagas <lchagas@umass.edu>

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/profiles', profiles);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
