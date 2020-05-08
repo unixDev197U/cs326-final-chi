56a417f28fe21db24cdaebd84307e4b78850da4d
nthurai <48607857+nthurai@users.noreply.github.com>
2020-05-07 21:47:40 -0400

    edit database.ts

--- config/database.ts
+++ config/database.ts
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
+const connectDB: Promise<void> = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useCreateIndex: true,
